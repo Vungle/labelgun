1cdeaaa8b5dc22231ab6871f8f25d25157148704
feelobot <felix.a.rod@gmail.com>
2016-02-04 13:06:14 -0800

    getting host

--- labelgun.go
+++ labelgun.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"os"
+	"time"
 	//"github.com/aws/aws-sdk-go/aws"
 	//"github.com/aws/aws-sdk-go/aws/session"
 	//"github.com/aws/aws-sdk-go/service/ec2"
@@ -10,6 +11,9 @@ import (
 )
 
 func main() {
-	node, _ := sh.Command("kubectl", "-s", os.Getenv("KUBE_MASTER"), "describe", "pod", os.Getenv("HOSTNAME")).Command("grep", "Node").Command("awk", "{print $2}").Command("sed", "s@/.*@@").Output()
-	fmt.Println(node)
+	for {
+		node, _ := sh.Command("kubectl", "-s", os.Getenv("KUBE_MASTER"), "describe", "pod", os.Getenv("HOSTNAME")).Command("grep", "Node").Command("awk", "{print $2}").Command("sed", "s@/.*@@").Output()
+		fmt.Println(string(node))
+		time.Sleep(time.Duration(5) * time.Second)
+	}
 }
